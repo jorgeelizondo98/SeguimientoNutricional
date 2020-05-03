6e55bdeec68058241be1d70099c72f0f2fd7a18e
mauriciogm97 <mauriciogm97@hotmail.com>
2020-05-03 17:12:14 -0500

    Facebook login

--- app/src/main/java/com/example/seguimientonutricional/ui/account/LoginActivity.java
+++ app/src/main/java/com/example/seguimientonutricional/ui/account/LoginActivity.java
@@ -15,6 +15,12 @@ import android.widget.Toast;
 
 import com.example.seguimientonutricional.MainActivity;
 import com.example.seguimientonutricional.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,7 +28,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -32,9 +40,10 @@ public class LoginActivity extends AppCompatActivity
 
   private static final String TAG = "LOGIN";
 
-  private static GoogleSignInClient mGoogleSignInClient;
+  private GoogleSignInClient mGoogleSignInClient;
+  private CallbackManager mFacebookCallbackManager;
   private static final int GOOGLE_SIGN_IN = 0;
-  private static final int FACEBOOK_SIGN_IN = 1;
+  private static final int FACEBOOK_SIGN_IN = 64206;
   private static final int LOGOUT = 2;
 
   private EmailLoginFragment emailLoginFragment;
@@ -65,6 +74,28 @@ public class LoginActivity extends AppCompatActivity
     emailRegisterFragment = EmailRegisterFragment.newInstance();
     current_fragment = emailRegisterFragment;
     switchEmailFragment(null);
+
+
+    mFacebookCallbackManager = CallbackManager.Factory.create();
+
+    LoginManager.getInstance().registerCallback(mFacebookCallbackManager,
+        new FacebookCallback<LoginResult>() {
+          @Override
+          public void onSuccess(LoginResult loginResult) {
+            // App code
+            handleFacebookAccessToken(loginResult.getAccessToken());
+          }
+
+          @Override
+          public void onCancel() {
+            // App code
+          }
+
+          @Override
+          public void onError(FacebookException exception) {
+            // App code
+          }
+        });
   }
 
   @Override
@@ -75,11 +106,11 @@ public class LoginActivity extends AppCompatActivity
     // the GoogleSignInAccount will be non-null.
     GoogleSignInAccount googleUser = GoogleSignIn.getLastSignedInAccount(this);
     // Check if user is signed in (non-null) and update UI accordingly.
-    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+    FirebaseUser firebaseOrFacebookUser = mAuth.getCurrentUser();
 
     if (googleUser != null) {
       updateUI();
-    } else if (firebaseUser != null) {
+    } else if (firebaseOrFacebookUser != null) {
       updateUI();
     }
   }
@@ -171,10 +202,11 @@ public class LoginActivity extends AppCompatActivity
       Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
       handleGoogleSignInResult(task);
     } else if (requestCode == FACEBOOK_SIGN_IN) {
-
+      mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
     } else if (requestCode == LOGOUT) {
       mAuth.signOut();
       mGoogleSignInClient.signOut();
+      LoginManager.getInstance().logOut();
     }
   }
 
@@ -190,6 +222,30 @@ public class LoginActivity extends AppCompatActivity
       // TODO: Get str from strings file.
       Toast.makeText(this, "Fallo login", Toast.LENGTH_SHORT).show();
     }
+  }
+
+  private void handleFacebookAccessToken(AccessToken token) {
+    Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+    AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+    mAuth.signInWithCredential(credential)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+              // Sign in success, update UI with the signed-in user's information
+              Log.d(TAG, "signInWithCredential:success");
+              FirebaseUser user = mAuth.getCurrentUser();
+              updateUI();
+            } else {
+              // If sign in fails, display a message to the user.
+              Log.w(TAG, "signInWithCredential:failure", task.getException());
+              // TODO: Get str from strings file.
+              Toast.makeText(LoginActivity.this, "Authentication failed.",
+                  Toast.LENGTH_SHORT).show();
+            }
+          }
+        });
   }
 
   private void updateUI() {

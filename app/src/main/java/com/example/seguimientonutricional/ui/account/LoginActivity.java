bc1f0ab93acc95649b7eed2ad777f64de97825c1
mauriciogm97 <mauriciogm97@hotmail.com>
2020-06-02 19:52:12 -0500

    Fixed facebook login

--- app/src/main/java/com/example/seguimientonutricional/ui/account/LoginActivity.java
+++ app/src/main/java/com/example/seguimientonutricional/ui/account/LoginActivity.java
@@ -23,6 +23,7 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -97,6 +98,10 @@ public class LoginActivity extends AppCompatActivity
     mFacebookCallbackManager = CallbackManager.Factory.create();
     LoginManager.getInstance().registerCallback(mFacebookCallbackManager,
         loginResultFacebookCallback);
+
+    // Initialize Facebook Login button
+    LoginButton loginButton = findViewById(R.id.login_button);
+    loginButton.setReadPermissions("email", "public_profile");
   }
   
   @Override
